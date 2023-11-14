3e4441217c58eae141cc5c3fd7e79bec695cd378
lucasbihannic <lucas.bihannic@master-developpement-logiciel.fr>
2023-11-14 11:04:25 +0100

    “Resolves_2”

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div style="text-align: left">
-    <button @click="getDataFromAPI">DO</button>
+      <button @click="getDataFromAPI">Request</button>
+    <activity v-if="this.data" :data="this.data"></activity>
   </div>
 </template>
 
 <script>
 
 import axios from "axios";
+import Activity from "./components/Activity.vue";
 
 export default {
   name: 'App',
+  components: {
+    Activity
+  },
   data () {
     return {
       data: null
@@ -19,7 +24,7 @@ export default {
     getDataFromAPI(){
       axios
           .get('https://www.boredapi.com/api/activity')
-          .then(response => {this.data = response
+          .then(response => {this.data = response.data
             console.log(this.data)})
     }
   }
---
+++ src/components/Activity.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div> You may want to ... </div>
+    <div> {{data.activity}} </div>
+    <div> {{data.type}} </div>
+    <div> {{translatePrice()}} </div>
+    <div> {{data.participants}} </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Activity",
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    translatePrice(){
+      if (this.data.price < 0.1){
+        return "€"
+      }
+      if (this.data.price < 0.4){
+        return "€€"
+      }
+      if (this.data.price < 0.7){
+        return "€€€"
+      }
+      return "€€€€"
+    }
+  },
+  props: {
+    data: {
+      type : Object,
+      required : true
+    }
+  },
+};
+</script>
+
+<style scoped>
+</style>
